fd7b4c8491ef6e9bba36b523d7f8bf37d45c5cf0
disha-cilans <disha.cilans@gmail.com>
2022-08-10 14:15:48 +0530

    update14

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,3 @@
 #!/bin/sh     
-sudo git pull origin master
-sudo pip3 install -r requirements.txt
-python3 manage.py makemigrations
-python3 manage.py migrate
-python3 manage.py collectstatic
-sudo systemctl restart nginx
-sudo systemctl restart gunicorn
+docker-compose down
+docker-compose up
